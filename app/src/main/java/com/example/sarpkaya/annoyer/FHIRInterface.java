ac1fd7d5eaf6ad27b3620abfae4f5730076ccad8
Vadim Peretokin <Vadim.Peretokin@nehta.gov.au>
2015-03-26 06:30:03 +1000

    Cleaned up function

--- app/src/main/java/com/example/sarpkaya/annoyer/FHIRInterface.java
+++ app/src/main/java/com/example/sarpkaya/annoyer/FHIRInterface.java
@@ -13,19 +13,40 @@ import ca.uhn.fhir.model.dstu2.resource.Medication;
 import ca.uhn.fhir.model.dstu2.resource.MedicationAdministration;
 import ca.uhn.fhir.model.dstu2.valueset.MedicationAdministrationStatusEnum;
 import ca.uhn.fhir.model.primitive.DateTimeDt;
-import java.util.List;
+import ca.uhn.fhir.rest.client.IGenericClient;
+
 
 /**
  * Created by VadimPeretokin on 25/03/15.
  */
 public class FHIRInterface {
     private static FHIRInterface ourInstance = new FHIRInterface();
+    private static FhirContext ctx = FhirContext.forDstu2();
+    String serverBase = "http://fhir-dev.healthintersections.com.au/open";
+    IGenericClient client = ctx.newRestfulGenericClient(serverBase);
 
     public static FHIRInterface getInstance() {
         return ourInstance;
     }
 
     private FHIRInterface() {
+    }
+
+    public Medication createMedication(long sctid, String name) {
+        Medication medication = new Medication();
+        medication.setName(name);
+        List<CodingDt> codedMedicine = new ArrayList<CodingDt>();
+        // setSystem is wrong for v3, need to find the right URI
+        //codedMedicine.add(new CodingDt().setDisplay(name).setCode(sctid.toString()).setSystem("http://nehta.gov.au/amt/v3"));
+        medication.setCode(new CodeableConceptDt().setCoding(codedMedicine));
+
+        return medication;
+    }
+
+    public MedicationAdministration createMedicationAdministration() {
+
+        MedicationAdministration medicationAdministration = new MedicationAdministration();
+        medicationAdministration.setEffectiveTime(new DateTimeDt().withCurrentTime()).setStatus(MedicationAdministrationStatusEnum.COMPLETED);
     }
 
     // SCTID 65384011000036101 in AMT is "Strepsils honey and lemon lozenge"
@@ -35,19 +56,12 @@ public class FHIRInterface {
         FhirContext ctx = FhirContext.forDstu2();
 
         // create a Medication
-        Medication medication = new Medication();
-        medication.setName(name);
-        List<CodingDt> codedMedicine = new ArrayList<CodingDt>();
-        // setSystem is wrong for v3, need to find the right URI
-        //codedMedicine.add(new CodingDt().setDisplay(name).setCode(sctid.toString()).setSystem("http://nehta.gov.au/amt/v3"));
-        medication.setCode(new CodeableConceptDt().setCoding(codedMedicine));
-
+        Medication medication = createMedication(sctid, name);
 
         // create an instance of a Medication being applied
-        MedicationAdministration medicationAdministration = new MedicationAdministration();
-        medicationAdministration.setEffectiveTime(new DateTimeDt().withCurrentTime()).setStatus(MedicationAdministrationStatusEnum.COMPLETED);
+        MedicationAdministration medicationAdministration = createMedicationAdministration();
 
-        // set the Medication taken to the Medication resource
+        // Put the Medication as a contained reference in the MedicationAdministration resource
         medicationAdministration.getMedication().setResource(medication);
 
         String jsonEncoded = ctx.newJsonParser().encodeResourceToString(medicationAdministration);

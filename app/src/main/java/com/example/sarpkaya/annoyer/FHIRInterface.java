b6496f57b6aa0b5db7dec8242dc4233a31808bf4
Vadim Peretokin <Vadim.Peretokin@nehta.gov.au>
2015-03-25 16:07:44 +1000

    Added FHIR interface

--- app/src/main/java/com/example/sarpkaya/annoyer/FHIRInterface.java
+++ app/src/main/java/com/example/sarpkaya/annoyer/FHIRInterface.java
@@ -1,7 +1,13 @@
 package com.example.sarpkaya.annoyer;
 
+import android.util.Log;
+
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.model.dstu2.resource.MedicationAdministration;
+import ca.uhn.fhir.model.dstu2.valueset.MedicationAdministrationStatusEnum;
+
 /**
- * Created by vadi on 25/03/15.
+ * Created by VadimPeretokin on 25/03/15.
  */
 public class FHIRInterface {
     private static FHIRInterface ourInstance = new FHIRInterface();
@@ -11,5 +17,19 @@ public class FHIRInterface {
     }
 
     private FHIRInterface() {
+    }
+
+    // SCTID 65384011000036101 in AMT is "Strepsils honey and lemon lozenge"
+    // creates a FHIR MedicationAdministration resource with the taken medication at current time
+    public void tookMedication(long sctid) {
+
+        FhirContext ctx = FhirContext.forDstu2();
+
+        MedicationAdministration medicationAdministration = new MedicationAdministration();
+
+        medicationAdministration.setStatus(MedicationAdministrationStatusEnum.COMPLETED);
+
+        String jsonEncoded = ctx.newJsonParser().encodeResourceToString(medicationAdministration);
+        Log.d("resource", jsonEncoded);
     }
 }

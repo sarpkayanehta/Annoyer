210c9c532c11aa1ff7cf7a5cf4ab847b5a3eba3e
Vadim Peretokin <Vadim.Peretokin@nehta.gov.au>
2015-03-26 12:38:39 +1000

    Put in some more real-world examples

--- app/src/main/java/com/example/sarpkaya/annoyer/PatientMedications.java
+++ app/src/main/java/com/example/sarpkaya/annoyer/PatientMedications.java
@@ -32,18 +32,54 @@ public class PatientMedications {
 
     private void populateMedications() {
         medicationModelList = new ArrayList<>();
+        MedicationModel medicationModel = new MedicationModel();
 
         //Medications
-        MedicationModel medicationModel = new MedicationModel();
-        medicationModel.setName("Strepsils honey and lemon lozenge");
-        medicationModel.setSctid(65384011000036101L);
-        DateTimeDt timeToTakeMedication = DateTimeDt.withCurrentTime();
-        medicationModel.setMedicationTimeToBeTaken(timeToTakeMedication);
-        medicationModelList.add(medicationModel);
-
         medicationModel = new MedicationModel();
         medicationModel.setName("memantine hydrochloride 20 mg tablet");
         medicationModel.setSctid(83436011000036100L);
+        medicationModel.setMedicationTimeToBeTaken(DateTimeDt.withCurrentTime());
+        medicationModelList.add(medicationModel);
+
+        medicationModel = new MedicationModel();
+        medicationModel.setName("econazole nitrate 1% (10 mg/g) cream");
+        medicationModel.setSctid(61893011000036101L);
+        medicationModel.setMedicationTimeToBeTaken(DateTimeDt.withCurrentTime());
+        medicationModelList.add(medicationModel);
+
+        medicationModel = new MedicationModel();
+        medicationModel.setName("idarubicin hydrochloride 10 mg/10 mL injection, vial");
+        medicationModel.setSctid(33681011000036108L);
+        medicationModel.setMedicationTimeToBeTaken(DateTimeDt.withCurrentTime());
+        medicationModelList.add(medicationModel);
+
+        medicationModel = new MedicationModel();
+        medicationModel.setName("dressing foam with silver 7.5 cm x 7.5 cm dressing");
+        medicationModel.setSctid(933241091000036109L);
+        medicationModel.setMedicationTimeToBeTaken(DateTimeDt.withCurrentTime());
+        medicationModelList.add(medicationModel);
+
+        medicationModel = new MedicationModel();
+        medicationModel.setName("lamotrigine 100 mg tablet");
+        medicationModel.setSctid(23610011000036100L);
+        medicationModel.setMedicationTimeToBeTaken(DateTimeDt.withCurrentTime());
+        medicationModelList.add(medicationModel);
+
+        medicationModel = new MedicationModel();
+        medicationModel.setName("temozolomide 5 mg capsule");
+        medicationModel.setSctid(22983011000036102L);
+        medicationModel.setMedicationTimeToBeTaken(DateTimeDt.withCurrentTime());
+        medicationModelList.add(medicationModel);
+
+        medicationModel = new MedicationModel();
+        medicationModel.setName("aspirin 500 mg + codeine phosphate hemihydrate 8 mg tablet: dispersible");
+        medicationModel.setSctid(61935011000036104L);
+        medicationModel.setMedicationTimeToBeTaken(DateTimeDt.withCurrentTime());
+        medicationModelList.add(medicationModel);
+
+        medicationModel = new MedicationModel();
+        medicationModel.setName("fluticasone propionate 500 microgram/actuation inhalation: powder for, actuation");
+        medicationModel.setSctid(22752011000036107L);
         medicationModel.setMedicationTimeToBeTaken(DateTimeDt.withCurrentTime());
         medicationModelList.add(medicationModel);
 
@@ -52,6 +88,13 @@ public class PatientMedications {
         medicationModel.setSctid(70194011000036104L);
         medicationModel.setMedicationTimeToBeTaken(DateTimeDt.withCurrentTime());
         medicationModelList.add(medicationModel);
+
+        medicationModel = new MedicationModel();
+        medicationModel.setName("moxonidine 200 microgram tablet");
+        medicationModel.setSctid(22059011000036104L);
+        medicationModel.setMedicationTimeToBeTaken(DateTimeDt.withCurrentTime());
+        medicationModelList.add(medicationModel);
+
 
     }
 }
